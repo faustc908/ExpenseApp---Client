523e9f70a7133f35608f974aacb926f7efd067d0
faustc908 <57776835+faustc908@users.noreply.github.com>
2020-10-15 22:51:34 -0400

    updates

--- src/components/AddTransaction/AddTransaction.js
+++ src/components/AddTransaction/AddTransaction.js
@@ -35,7 +35,7 @@ export const AddTransaction = () => {
         </div>
         <div className="form-control">
           <label htmlFor="amount"
-          >Amount (for subtractions use -):
+          >Amount (for expenses use -):
           </label>
           <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
         </div>
